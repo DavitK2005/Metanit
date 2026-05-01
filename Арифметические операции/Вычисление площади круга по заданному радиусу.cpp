b91fd257e82474103f5e5fca335cb5b1ab2ee195
Davit Kirakosyan <94119776+DavitK2005@users.noreply.github.com>
2026-05-02 01:04:45 +0400

    Add files via upload

---
+++ Арифметические операции/Вычисление площади круга по заданному радиусу.cpp
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая запрашивает у пользователь радиус круга и, используя полученный радиус, вычисляет площадь круга.
+
+#include <iostream>
+
+int main() {
+	const float pi{ 3.14159 };
+
+	float radius{};
+	std::cout << "Enter a number of radius: ";
+	std::cin >> radius;
+
+	const float S{ pi * radius * radius };
+	std::cout << "S = " << S;
+}
